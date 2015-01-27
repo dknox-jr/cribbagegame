971cb5b6fb01e93bcc689b3339cc7354f7e29026
dknox-jr <dknox.jr@gmail.com>
2015-01-26 21:52:26 -0800

    moved functions to class based for cleaner and smoother code.  Able to deal hands, throw away cards, cut common card, play cards, alternate turns, etc.

--- classes.py
+++ classes.py
@@ -1,59 +1,218 @@
+import random
+import cribbage
+
+
 class Player():
-    def __init__(self, turn="No", dealer="No", score=0):
+    def __init__(self, one_card, turn="No", dealer="No", said_go="No", score=0):
+        self.one_card = one_card
         self.score = score
         self.dealer = dealer
         self.turn = turn
+        self.said_go = said_go
         self.hand = []
         self.crib = []
+        self.played = []
 
-    def altturn(self):
-        if self.turn == "No":
-            self.turn = "Yes"
-        elif self.turn == "Yes":
-            self.turn = "No"
-
-    def altdeal(self):
+    def alt_deal(self):
         if self.dealer == "No":
             self.dealer = "Yes"
         elif self.dealer == "Yes":
             self.dealer = "No"
 
-    def move_peg(self):
+    def alt_turn(self):
+        if self.turn == "No":
+            self.turn = "Yes"
+        elif self.turn == "Yes":
+            self.turn = "No"
+
+    def check_score(self):
+        print "Your score is: ", self.score
+        print "Your opponent's score is: ", opponent.score
+        self.display_options()
+
+    def cut_a_card(self):
+        self.one_card = random.choice(game.deck)
+        return self.one_card
+
+    def discard_to_crib(self):
+        print "Choose which cards to pass to the crib"
+        for card in self.hand:
+            print str(self.hand.index(card) + 1) + ")", card
+        print "select a card by typing its corresponding number and press enter"
+        discard = raw_input(">>>")
+        if int(discard) <= len(self.hand):
+            if self.dealer == "Yes":
+                self.crib.append(self.hand.pop(int(discard) - 1))
+            if opponent.dealer == "Yes":
+                opponent.crib.append(self.hand.pop(int(discard) - 1))
+        else:
+            print "Sorry, that wasn't an option."
+        if len(self.crib) == 2:
+            print "You threw in: "
+            for card in self.crib:
+                print card
+        elif len(opponent.crib) == 2:
+            print "You threw in: "
+            for card in opponent.crib:
+                print card
+
+    def display_options(self):
+        print """Here are your options:
+        """
+        print "[P]lay a Card"
+        print "Say [G]o"
+        # print "[O]rganize Hand"
+        print "Check [S]core"
+        print "Review [C]ards Played"
+        # print "[Q]uit Game"
+        action = str(raw_input(">>>"))
+        if action == "P" or action == "p":
+            self.play_card()
+        elif action == "G" or action == "g":
+            self.say_go()
+        # elif action == "O" or action == "o":
+        #     organize_hand()
+        elif action == "S" or action == "s":
+            self.check_score()
+        elif action == "C" or action == "c":
+            self.review_cards_played()
+        # elif action == "Q" or action == "q":
+        #     game.end_game()
+        else:
+            print "Sorry, that wasn't an option."
+            self.display_options()
+
+    def end_turn(self):
+        self.alt_turn()
+        opponent.alt_turn()
+        opponent.take_turn()
+
+    def move_peg(self, points):
+        self.score += points
         if self.score > 120:
             print "You Won!"
-        else:
-            self.score += 1
 
-current_player = Player(turn="No", dealer="No", score=0)
+    def play_card(self):
+        print "select a card by typing its corresponding number and press enter"
+        print "or [R]eturn to options"
+        for card in self.hand:
+            print str(self.hand.index(card) + 1) + ")", card
+        play = raw_input(">>>")
+        if play == "R" or play == "r":
+            self.display_options()
+        else:
+            try:
+                if int(play) <= len(self.hand):
+                    self.played.append(self.hand.pop(int(play) - 1))
+                    cycle.lcp = self.played[-1]
+                    print "You played ", cycle.lcp
+                    self.end_turn()
+                else:
+                    print "Sorry, that wasn't an option."
+                    self.play_card()
+            except ValueError:
+                print "Sorry, that wasn't an option."
+                self.play_card()
+
+    def review_cards_played(self):
+        print cycle.lcp
+        self.display_options()
+
+    def say_go(self):
+        self.said_go = "Yes"
+        self.end_turn()
+
+    def take_turn(self):
+        if len(self.hand) > 0:
+            print "Your hand is: "
+            for card in self.hand:
+                print card
+        self.display_options()
 
 
 class Bot():
-    def __init__(self, turn="No", dealer="No", score=0):
+    def __init__(self, one_card, turn="No", dealer="No", said_go="No", score=0):
+        self.one_card = one_card
         self.score = score
         self.dealer = dealer
         self.turn = turn
+        self.said_go = said_go
         self.hand = []
         self.crib = []
+        self.played = []
 
-    def altturn(self):
-        if self.turn == "No":
-            self.turn = "Yes"
-        elif self.turn == "Yes":
-            self.turn = "No"
-
-    def altdeal(self):
+    def alt_deal(self):
         if self.dealer == "No":
             self.dealer = "Yes"
         elif self.dealer == "Yes":
             self.dealer = "No"
 
-    def move_peg(self):
-        self.score += 1
+    def alt_turn(self):
+        if self.turn == "No":
+            self.turn = "Yes"
+        elif self.turn == "Yes":
+            self.turn = "No"
 
-opponent = Bot(turn="No", dealer="No", score=0)
+    def cut_a_card(self):
+        self.one_card = random.choice(game.deck)
+        return self.one_card
+
+    def discard_to_crib(self):
+        if self.dealer == "Yes":
+            self.crib.append(self.hand.pop())
+        if player.dealer == "Yes":
+            player.crib.append(self.hand.pop())
+
+    def end_turn(self):
+        self.alt_turn()
+        player.alt_turn()
+        player.take_turn()
+
+    def move_peg(self, points):
+        self.score += points
+        if self.score > 120:
+            print "Your opponent won :("
+
+    def take_turn(self):
+        self.played.append(self.hand.pop())
+        cycle.lcp = self.played[-1]
+        print "Your opponent played: ", self.played[-1]
+        self.end_turn()
 
 
-class Round():
+class Game():
+    def __init__(self, players, bots, status):
+        self.players = players
+        self.bots = bots
+        self.status = status
+        self.deck = cribbage.cards
+
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
+
+    def compare_cut(self):
+        print "You showed " + str(player.one_card) + " and your opponent showed " + str(opponent.one_card) + "."
+        test = cribbage.Card.__cmp__(player.one_card, opponent.one_card)
+        if test == 1:
+            print "You will take the first deal."
+            player.alt_deal()
+            opponent.alt_turn()
+        else:
+            print "Your opponent will take the first deal."
+            opponent.alt_deal()
+            player.alt_turn()
+        self.deal_hand()
+
+    def deal_hand(self):
+        # self.shuffle_deck()
+        while len(player.hand) < 6:
+            player.hand.append(self.deck.pop())
+        while len(opponent.hand) < 6:
+            opponent.hand.append(self.deck.pop())
+        print player.hand
+
+
+class Cycle():
     def __init__(self, rid=0, fuc=None, lcp=None, tot=0):
         self.rid = rid
         self.lcp = lcp
@@ -67,9 +226,26 @@ class Round():
         self.lcp = []
 
     def new_fuc(self):
-        self.fuc = []
-        if len(self.fuc) > 0:
-            self.fuc.pop()
+        if player.dealer == "Yes":
+            player.cut_a_card()
+            self.fuc = player.one_card
+            print "The common card is: ", str(self.fuc)
+            if "Jack" in str(self.fuc):
+                player.move_peg(2)
+                print "2 points for you!"
+        elif opponent.dealer == "Yes":
+            opponent.cut_a_card()
+            self.fuc = opponent.one_card
+            print "The common card is: ", str(self.fuc)
+            if "Jack" in str(self.fuc):
+                opponent.move_peg(2)
+                print "2 points for your opponent :("
 
     def new_tot(self):
         self.tot += 1
+
+
+game = Game(players=1, bots=1, status=None)
+player = Player(one_card=None, turn="No", dealer="No", score=0)
+opponent = Bot(one_card=None, turn="No", dealer="No", score=0)
+cycle = Cycle(rid=0, fuc=None, lcp=None, tot=0)
--- gameplay.py
+++ gameplay.py
@@ -1,155 +1,32 @@
-import random
-import cribbage
 import classes
 
 
 def start_game():
-    print "Cut to see who will deal first."
-    cut_for_deal()
+    game_setup()
 
 
-def shuffle_deck():
-    random.shuffle(cribbage.cards)
-    return cribbage.cards
+def game_setup():
+    classes.game.shuffle_deck()
+    classes.player.cut_a_card()
+    classes.opponent.cut_a_card()
+    classes.game.compare_cut()
+    start_hand()
 
 
-def cut_for_deal():
-    shuffle_deck()
-    player_card = cribbage.cards.pop()
-    computer_card = cribbage.cards.pop()
-    print "You showed " + str(player_card) + " and your opponent showed " + str(computer_card) + "."
-    compare_cut(player_card, computer_card)
-    return player_card, computer_card
+def start_hand():
+    while len(classes.player.hand) > 4:
+        classes.player.discard_to_crib()
+    while len(classes.opponent.hand) > 4:
+        classes.opponent.discard_to_crib()
+    classes.cycle.new_fuc()
+    play_hand()
 
 
-def compare_cut(player_card, computer_card):
-    test = cribbage.Card.__cmp__(player_card, computer_card)
-    if test == 1:
-        print "You will take the first deal."
-        classes.current_player.altdeal()
-        classes.opponent.altturn()
-        deal_hand()
-    else:
-        print "Your opponent will take the first deal."
-        classes.opponent.altdeal()
-        classes.current_player.altturn()
-        deal_hand()
-
-
-def deal_hand():
-    shuffle_deck()
-    myhand = classes.current_player.hand
-    opphand = classes.opponent.hand
-
-    while len(myhand) < 6:
-        myhand.append(cribbage.cards.pop())
-    while len(opphand) < 6:
-        opphand.append(cribbage.cards.pop())
-
-    print "Your hand is: "
-    for card in myhand:
-        print card
-    face_up(myhand, opphand)
-    return myhand, opphand
-
-
-def face_up(myhand, opphand):
-    up_card = str(cribbage.cards.pop())
-    print "The Common card is: ", up_card
-    if "Jack" in up_card:
-        if classes.current_player.dealer == "Yes":
-            for i in range(2):
-                classes.current_player.move_peg()
-            # classes.current_player.move_peg()
-            print classes.current_player.score
-            print classes.opponent.score
-        if classes.opponent.dealer == "Yes":
-            classes.opponent.move_peg()
-            classes.opponent.move_peg()
-            print classes.opponent.score
-            print classes.current_player.score
-        print "2 points!"
-    play_hand(myhand, opphand)
-    return up_card
-
-
-def play_hand(myhand, opphand):
-    if classes.current_player.turn == "Yes":
-        display_options(myhand, opphand)
+def play_hand():
+    if classes.player.turn == "Yes":
+        classes.player.take_turn()
     elif classes.opponent.turn == "Yes":
-        opponent_turn(myhand, opphand)
-    return myhand, opphand
-
-
-def display_options(myhand, opphand):
-    print """Here are your options:
-        """
-    if len(myhand) > 0:
-        print "[P]lay a card"
-        print "say [G]o"
-        print "[O]rganize hand"
-    action = str(raw_input(">>>"))
-    if action == "P" or action == "p":
-        play_card(myhand, opphand)
-    elif action == "G" or action == "g":
-        say_go(myhand, opphand)
-    elif action == "O" or action == "o":
-        organize_hand(myhand, opphand)
-    else:
-        print "Sorry, that wasn't an option."
-        display_options(myhand, opphand)
-
-
-def play_card(myhand, opphand):
-    for card in myhand:
-        print str(myhand.index(card) + 1) + ")", card
-    print "select a card by typing its corresponding number and press enter"
-    play = raw_input(">>>")
-    if int(play) <= len(myhand):
-        my_played = myhand.pop(int(play) - 1)
-        print "You played ", my_played
-        end_player_turn(myhand, opphand)
-        return myhand, my_played
-    else:
-        print "Sorry, that wasn't an option."
-        play_card(myhand, opphand)
-
-
-def opponent_turn(myhand, opphand):
-    opp_play = opphand.pop()
-    print "Your opponent played: ", opp_play
-    end_opponent_turn(myhand, opphand)
-    return opp_play, opphand
-
-
-def end_player_turn(myhand, opphand):
-    classes.current_player.altturn()
-    classes.opponent.altturn()
-    opponent_turn(myhand, opphand)
-
-
-def end_opponent_turn(myhand, opphand):
-    classes.opponent.altturn()
-    classes.current_player.altturn()
-    display_options(myhand, opphand)
-
-
-def organize_hand(myhand, opphand):
-    for card in myhand:
-        o_hand = []
-        if cribbage.Card.__cmp__(card, card[-1]) == -1:
-            o_hand.append(myhand.pop())
-        else:
-            o_hand.append(myhand.pop(0))
-        myhand = o_hand
-        return myhand
-    display_options(myhand, opphand)
-
-
-def say_go(myhand, opphand):
-    classes.current_player.altturn()
-    classes.opponent.altturn()
-    play_hand(myhand, opphand)
+        classes.opponent.take_turn()
 
 
 start_game()
